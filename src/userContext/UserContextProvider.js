99662a1e52f8bf58c4d9d8e0e04517730f854be4
CaioPedras <caio.vp@gmail.com>
2022-08-05 16:52:13 -0300

    url heroku

--- src/userContext/UserContextProvider.js
+++ src/userContext/UserContextProvider.js
@@ -2,7 +2,7 @@ import React from "react";
 import { UserContext } from "./userContext.js";
 import { useState } from "react";
 export default function UserContextProvider(props) {
-  const URL = "http://localhost:4000";
+  const URL = "https://shortly-t6.herokuapp.com";
   const [token, setToken] = useState();
   return (
     <UserContext.Provider value={{ URL, token, setToken }}>
